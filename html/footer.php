e87b50fb7194471600ae7f9009e23857c594f6dd
nicoorona95 <nicolasoronards@gmail.com>
2019-11-23 20:55:40 -0300

    retoques finales

--- html/footer.php
+++ html/footer.php
@@ -1,49 +1,68 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php include_once "estilo.php" ?>
+    <link href="https://fonts.googleapis.com/css?family=Assistant:400,700|Roboto&display=swap" rel="stylesheet">
+    <title>FAQ</title>
+  </head>
+</head>
+<body>
+  <footer class="footerstyle">
 
-  <footer class="py-5 bg-dark footerstyle">
+    <div class="container-fluid">
+      <div class="row">
 
-  <div class="row" class="container-fluid">
-    <div class="col col-sm-4 col-md-4 col-lg-4 col-xl-4">
-      <div class="m-0 text-center text-white">Mapa del Sitio</div>
-      <div class="navfoot">
-          <ul>
-              <li><a href="home.php" title="Página principal de entre Diagonales">Home</a></li>
-              <li><a href="about.php" title="Conocenos">Sobre Nosotros</a></li>
-              <li><a href="listaproductos.php" title="entre Diagonales - Nuestras excursiones">Excursiones</a></li>
-              <li><a href="fyq.php" title="¿Preguntas?">Preguntas frecuentes</a></li>
-              <li><a href="contact.php" title="Contactanos">Contacto</a></li>
-              <li><a href="registro.php" title="Registrate y enterate de las últimas novedades y promociones">Registrarse</a></li>
-              <li><a href="login.php" title="Logueate">Login</a></li>
-          </ul>
+
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <div class="m-0"><p class="h4 text-center text-uppercase m-3"><strong>MAPA DEL SITIO</strong></p></div>
+          <div class="navfoot">
+              <ul>
+                  <li><a href="home.php" title="Página principal de entre Diagonales">Home</a></li>
+                  <li><a href="about.php" title="Conocenos">Sobre Nosotros</a></li>
+                  <li><a href="listaproductos.php" title="entre Diagonales - Nuestras excursiones">Excursiones</a></li>
+                  <li><a href="fyq.php" title="¿Preguntas?">Preguntas frecuentes</a></li>
+                  <li><a href="contact.php" title="Contactanos">Contacto</a></li>
+                  <li><a href="registro.php" title="Registrate y enterate de las últimas novedades y promociones">Registrarse</a></li>
+                  <li><a href="login.php" title="Logueate">Login</a></li>
+              </ul>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+          <div class="m-0 "><p class="h4 text-center text-uppercase m-3"><strong>SOCIAL MEDIA</strong></p></div>
+          <div class="navfoot">
+              <ul>
+                  <li><a href="#" title="Facebook de entre Diagonales">Facebook</a></li>
+                  <li><a href="#" title="Instagram de entre Diagonales">Instagram</a></li>
+                  <li><a href="#" title="Twitter de entre Diagonales">Twitter</a></li>
+              </ul>
+
+          </div>
+        </div>
+
+        <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+           <div class="m-0"><p class="h4 text-center text-uppercase m-3"><strong> POLÍTICA DE USO</strong></p></div>
+            <div class="navfoot">
+                <ul>
+                    <li><a href="#" title="Condiciones">Condiciones generales</a></li>
+                    <li><a href="#" title="Aviso legal">Aviso legal</a></li>
+                    <li><a href="#" title="Política de privacidad de entre Diagonales">Política de privacidad</a></li>
+                </ul>
+            </div>
+        </div>
       </div>
     </div>
-    <div class="col col-sm-4 col-md-4 col-lg-4 col-xl-4">
-                <div class="m-0 text-center text-white">Social Media</div>
-      <div class="navfoot">
-          <ul>
-              <li><a href="#" title="Facebook de entre Diagonales">Facebook</a></li>
-              <li><a href="#" title="Instagram de entre Diagonales">Instagram</a></li>
-              <li><a href="#" title="Twitter de entre Diagonales">Twitter</a></li>
-          </ul>
 
-      </div>
+
+    <div class="container">
+      <p class="m-0 text-center">Copyright &copy; ENTRE DIAGONALES. 2019 </p>
     </div>
 
-    <div class="col col-sm-4 col-md-4 col-lg-4 col-xl-4">
-     <div class="m-0 text-center text-white">Políticas de uso</div>
-      <div class="navfoot">
-          <ul>
-              <li><a href="#" title="Condiciones">Condiciones generales</a></li>
-              <li><a href="#" title="Aviso legal">Aviso legal</a></li>
-              <li><a href="#" title="Política de privacidad de entre Diagonales">Política de privacidad</a></li>
-          </ul>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="container">
-    <p id="texto" class="m-0 text-center text-white">Copyright &copy; ENTRE DIAGONALES. 2019 </p>
-  </div>
-
-  <!-- /.container -->
+    <!-- /.container -->
   </footer>
+</body>
+</html>
