6b6aff87b4b3cf7356dd65917e92592a3d256025
Agustín Fernández <anf90.e@gmail.com>
2019-11-23 11:33:23 -0300

    agrego footer

--- html/footer.php
+++ html/footer.php
@@ -1,6 +1,49 @@
-<footer class="py-5 bg-dark">
-<div class="container">
-  <p id="texto" class="m-0 text-center text-white">Copyright &copy; ENTRE DIAGONALES. 2019 </p>
-</div>
-<!-- /.container -->
-</footer>
+
+  <footer class="py-5 bg-dark" class="footerstyle">
+
+  <div class="row" class="container-fluid">
+    <div class="col" class="col-md-4 col-lg-4">
+      <div class="m-0 text-center text-white">Mapa del Sitio</div>
+      <div class="navfoot">
+          <ul>
+              <li><a href="home.php" title="Página principal de entre Diagonales">Home</a></li>
+              <li><a href="about.php" title="Conocenos">Sobre Nosotros</a></li>
+              <li><a href="listaproductos.php" title="entre Diagonales - Nuestras excursiones">Excursiones</a></li>
+              <li><a href="fyq.php" title="¿Preguntas?">Preguntas frecuentes</a></li>
+              <li><a href="contact.php" title="Contactanos">Contacto</a></li>
+              <li><a href="registro.php" title="Registrate y enterate de las últimas novedades y promociones">Registrarse</a></li>
+              <li><a href="login.php" title="Logueate">Login</a></li>
+          </ul>
+      </div>
+    </div>
+    <div class="col" class="col-md-4 col-lg-4">
+                <div class="m-0 text-center text-white">Social Media</div>
+      <div class="navfoot">
+          <ul>
+              <li><a href="#" title="Facebook de entre Diagonales">Facebook</a></li>
+              <li><a href="#" title="Instagram de entre Diagonales">Instagram</a></li>
+              <li><a href="#" title="Twitter de entre Diagonales">Twitter</a></li>
+          </ul>
+
+      </div>
+    </div>
+
+    <div class="col" class="col-md-4 col-lg-4">
+     <div class="m-0 text-center text-white">Políticas de uso</div>
+      <div class="navfoot">
+          <ul>
+              <li><a href="#" title="Condiciones">Condiciones generales</a></li>
+              <li><a href="#" title="Aviso legal">Aviso legal</a></li>
+              <li><a href="#" title="Política de privacidad de entre Diagonales">Política de privacidad</a></li>
+          </ul>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container">
+    <p id="texto" class="m-0 text-center text-white">Copyright &copy; ENTRE DIAGONALES. 2019 </p>
+  </div>
+
+  <!-- /.container -->
+  </footer>
